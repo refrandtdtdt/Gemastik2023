f00269ac9d4073a7c019ab07b732a52a9e86c829
ZeevHaydar <nzhifhaidar@gmail.com>
2023-06-14 00:44:01 +0700

    submit malam ini

---
+++ Assets/Script/Rama.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rama : Player
+{
+    
+    public Rama()
+    {
+        SetId(1);
+        SetDescription("Karakter utama dari ini, memiliki senjata panah idk the name lmao");
+    }
+    // Start is called before the first frame update
+/*
+    void Start()
+    {
+        
+    }
+*/
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
