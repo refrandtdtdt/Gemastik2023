77f2e1777d768d79e83654fcc312e1bcc210c619
ZeevHaydar <nzhifhaidar@gmail.com>
2023-06-19 17:42:15 +0700

    add jump limit

--- Assets/Script/Entities/Player.cs
+++ Assets/Script/Entities/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Player : Entity
 {
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private float speed;
     [SerializeField] private float jumpDistance;
     protected Rigidbody2D rb;
@@ -22,9 +23,12 @@ public class Player : Entity
             transform.position += new Vector3(speed, 0);
         }
     }
+    /**
+     * Jump action for player
+     */
     public void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (CheckGround() && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Kuduna Loncat euy");
             rb.AddForce(new Vector2(0, jumpDistance), ForceMode2D.Impulse);
@@ -51,5 +55,26 @@ public class Player : Entity
         this.jumpDistance = jumpDistance;
     }
 
+    private bool CheckGround()
+    {
+        Collider2D playerCollider = GetComponent<BoxCollider2D>();
+        Vector2 raycastPos = playerCollider.bounds.center;
+        float raycastDist = playerCollider.bounds.extents.y + 0.1f;
 
+        RaycastHit2D hit = Physics2D.BoxCast(raycastPos, playerCollider.bounds.size, 0f, Vector2.down, raycastDist, layerMask);
+        Color rayColor;
+        if (hit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+
+        }
+        Debug.DrawRay(raycastPos + new Vector2(playerCollider.bounds.extents.x, 0), Vector2.down * raycastDist, rayColor);
+        Debug.DrawRay(raycastPos - new Vector2(playerCollider.bounds.extents.x, 0), Vector2.down * raycastDist, rayColor);
+        Debug.DrawRay(raycastPos - new Vector2(playerCollider.bounds.extents.x, raycastDist), Vector2.right * raycastDist*2, rayColor);
+        return hit.collider != null;
+    }
 }
