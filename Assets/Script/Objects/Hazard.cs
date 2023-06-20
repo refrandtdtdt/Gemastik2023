f0a3f05555a798bb62c49b5d385d3be95bbf7320
Ferdeh <16521246@mahasiswa.itb.ac.id>
2023-06-20 18:19:04 +0700

    akhirnya kerja anjir
    hazard ngedamage terus2an dengan cooldown

--- Assets/Script/Objects/Hazard.cs
+++ Assets/Script/Objects/Hazard.cs
@@ -5,11 +5,48 @@ using UnityEngine;
 public class Hazard : Object
 {
     [SerializeField] int damage;
+    [SerializeField] float cd;
+    float currCd = 0;
+    bool hit = false;
+    Player player;
+
+    private void Update()
+    {
+        if (hit)
+        {
+            if(currCd <= 0)
+            {
+                player.SetHealth(player.GetHealth() - damage);
+                currCd = cd;
+            }
+            else
+            {
+                currCd -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if(currCd > 0)
+            {
+                currCd -= Time.deltaTime;
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.TryGetComponent(out Entity ent))
+        if (collision.gameObject.TryGetComponent(out Player p))
         {
-            ent.SetHealth(ent.GetHealth() - damage);
+            player = p;
+            hit = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player p))
+        {
+            player = p;
+            hit = false;
         }
     }
 }
