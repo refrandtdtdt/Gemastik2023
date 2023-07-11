90dad6d780ec371e14bde147636ef6f1d7a2913c
ZeevHaydar <nzhifhaidar@gmail.com>
2023-07-11 20:35:18 +0700

    tulung aku

--- Assets/Script/Objects/Box/Box.cs
+++ Assets/Script/Objects/Box/Box.cs
@@ -4,36 +4,23 @@ using UnityEngine;
 
 public abstract class Box : MonoBehaviour,IMoveable
 {
-    //method override
-    private void OnTriggerStay2D(Collider2D collider)
-    {
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            if (collider.gameObject.TryGetComponent<Player>(out _))
-            {
-                Debug.Log("Whoops!");
-                Interact();
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-
-        }
-    }
+    protected bool beingPull = false;
+    protected bool inside = false;
+    protected Player player;
+    protected Rigidbody2D rb;
+    protected LayerMask boundaryLayer;
+    protected Vector3 deltaPlayerBox;
+    
     public void Interact()
     {
-        
+        transform.position = player.transform.position - deltaPlayerBox;
+
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void hitBoundary()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 origin = rb.position;
+        //float rayCastDist = rb.position
+        //RaycastHit2D hit = Physics2D.BoxCast() 
     }
 }
--- Assets/Script/Objects/Box/SolidBox.cs
+++ Assets/Script/Objects/Box/SolidBox.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class SolidBox : Box
@@ -7,12 +9,47 @@ public class SolidBox : Box
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponentInChildren<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inside && Input.GetKey(KeyCode.LeftShift)) 
+        {
+            beingPull = true;
+            deltaPlayerBox = player.transform.position - transform.position;
+        }
+        else
+        {
+            beingPull = false;
+            deltaPlayerBox = Vector3.zero;
+        }
+        if (beingPull)
+        {
+            Interact();
+        }
+        else
+        {
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            inside = true;
+            this.player = player;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out _))
+        {
+            inside = false;
+            this.player = null;
+        }
+
+    }
+
 }
