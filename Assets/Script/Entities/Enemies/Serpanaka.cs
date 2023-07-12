ebe64525231a91090b9eee773e03f09e3491b13c
ZeevHaydar <nzhifhaidar@gmail.com>
2023-07-12 16:01:41 +0700

    Update Serpanaka.cs

--- Assets/Script/Entities/Enemies/Serpanaka.cs
+++ Assets/Script/Entities/Enemies/Serpanaka.cs
@@ -11,11 +11,12 @@ public class Serpanaka : Enemy
         SetDescription("Serpanakan, raksasa kuat adik dari rahwana");
         Speed = 6f;
         JumpDistance = 8;
-        SetMaxHealth(100);
-        SetHealth(100);
+        SetMaxHealth(1000);
+        SetHealth(1000);
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         Scale = transform.localScale;
+        AttackPower = 20;
     }
 
     // Update is called once per frame
@@ -31,6 +32,24 @@ public class Serpanaka : Enemy
 
     public void BasicAttack()
     {
+        Collider2D[] players = Physics2D.OverlapBoxAll(attackPoint.transform.position, new Vector2(AttackRadius / 2, AttackRadius), 0f, playerMask);
+        foreach (Collider2D playerCollider in players)
+        {
+            Debug.Log("Hit Player");
+            if (playerCollider.TryGetComponent<Player>(out Player player))
+            {
+                player.SetHealth(player.GetHealth() - AttackPower);
+            }
+        }
+    }
 
+    public void SkillAttack()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(attackPoint.transform.position, new Vector3(AttackRadius/2, AttackRadius, 1));
     }
 }
